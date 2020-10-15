5d1c8428b7a02e08364a529a3c9d466e3d80e139
Jermaine Maxey <jermainemaxey21@gmail.com>
2020-10-15 00:34:29 -0500

    Decorations for ruby project
    Nice and set up with decorations for the ruby project

---
+++ Assets/Scripts/RubyController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RubyController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector2 position = transform.position;
+        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 3.0f * vertical * Time.deltaTime;
+        transform.position = position;
+    }
+}
