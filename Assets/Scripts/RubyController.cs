0ed6a0b2b412a4194ac49e015ca6fd6073938d95
Jermaine Maxey <jermainemaxey21@gmail.com>
2020-10-29 09:26:17 -0500

    health bar included
    healthbar included

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -85,7 +85,8 @@ public class RubyController : MonoBehaviour
         }
 
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
-        Debug.Log(currentHealth + "/" + maxHealth);
+
+        UIHealthBar.instance.SetValue(currentHealth / (float)maxHealth);
     }
 
     void Launch()
